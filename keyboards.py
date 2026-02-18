c1a2b498c0994fe5343838cba07399200bcaacb0
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-02-18 20:40:05 +0300

    Переписал приветственное сообщение бота и убрал лишние кнопки из главного меню

--- keyboards.py
+++ keyboards.py
@@ -2,7 +2,6 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 def main_menu():
     Keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    Keyboard.add(KeyboardButton("Что это"), KeyboardButton("Правила"))
-    Keyboard.add(KeyboardButton("Оформить вывоз"), KeyboardButton("Адреса складов"))
-    Keyboard.add(KeyboardButton("Мои заказы"), KeyboardButton("Помощь"))
+    Keyboard.add(KeyboardButton("Правила хранения"), KeyboardButton("Мои заказы"))
+    Keyboard.add(KeyboardButton("Уже храню вещи"), KeyboardButton("Хочу хранить вещи"))
     return Keyboard
